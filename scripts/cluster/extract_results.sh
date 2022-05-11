9b1784e4048331478824b4553fa56c2261a5ced6
Wolf <wolf2wulf@gmail.com>
2022-05-11 17:07:51 +0200

    MAGUS_CP update + added runlim to experiment scripts

--- scripts/cluster/extract_results.sh
+++ scripts/cluster/extract_results.sh
@@ -27,8 +27,11 @@ for msa in $(ls -1 results); do
         SPFN=$(echo "$fastsp_output" | grep 'SPFN' | grep -Eo '[+-]?[0-9]+([.][0-9]+)?')
         SPFP=$(echo "$fastsp_output" | grep 'SPFP' | grep -Eo '[+-]?[0-9]+([.][0-9]+)?')
 
+        # Extract memory used
+        used_memory = $(cat results/${msa}/${trace_methods_arr[$index]}_runlim.txt | grep 'space:' | grep -Eo '[+-]?[0-9]+([.][0-9]+)?');  
+
         # Write results
-        # msa name, method, trace running time, total running time, SPFN, SPFP
-        printf "${msa},${trace_methods_arr[$index]},${trace_times_arr[$index]},${run_times_arr[$index]},${SPFN},${SPFP}\n" >> results.csv
+        # msa name, method, trace running time, total running time, used memory, SPFN, SPFP
+        printf "${msa},${trace_methods_arr[$index]},${trace_times_arr[$index]},${run_times_arr[$index]},${used_memory}, ${SPFN},${SPFP}\n" >> results.csv
     done
 done
